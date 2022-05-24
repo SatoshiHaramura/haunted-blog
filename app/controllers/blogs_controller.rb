b1c971d71f2fdebb953aeb7dd30a9c0ac40ac05e
Satoshi Haramura <satoshi.haramura@gmail.com>
2022-05-24 14:15:27 +0900

    Add authentication and authorization for secret blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,9 @@ class BlogsController < ApplicationController
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    authorize_user if @blog.secret
+  end
 
   def new
     @blog = Blog.new
